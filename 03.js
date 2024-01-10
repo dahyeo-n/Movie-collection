57d07dda6f176c8d3eaa8fd1d6ccaafb5f452009
진다현 <yougit config --global user.email wlsekgus12@gmail.comgit config --global user.name >
2024-01-10 12:58:05 +0900

    test

--- 03.js
+++ 03.js
@@ -38,3 +38,14 @@ const friends = [
   })
   
   console.log('이름은 ', happyPeoplename);
+
+
+  // 평균 구하기
+  function solution(arr) {
+    let sum = 0;
+    for (let i = 0; i < arr.length; i++) {
+      sum += arr[i];
+    }
+    let avg = sum / arr.length;
+    return avg;
+  }
